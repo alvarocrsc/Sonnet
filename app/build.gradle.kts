6e3fb1cc6d812288e585b2bab6c708ecad8b684d
alvarocrsc <alvarocrsc@gmail.com>
2025-12-15 14:52:20 +0100

    First Stats Design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout.core)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.cardview)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
